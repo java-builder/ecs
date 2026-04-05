4888e975205107d01768a8c14613c7d62b59fd06
duclk <lekhanhduccc@gmail.com>
2026-04-05 19:27:04 +0700

    Add CD pipeline, task definition, and environment message configuration.

--- src/main/java/com/javabuilder/ecs/ECSController.java
+++ src/main/java/com/javabuilder/ecs/ECSController.java
@@ -1,14 +1,18 @@
 package com.javabuilder.ecs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ECSController {
 
+    @Value("${app.message}")
+    private String message;
+
     @GetMapping
     public String ecsContainer() {
-        return "Hello from ECS! Instance: " + System.getenv("HOSTNAME");
+        return message;
     }
 
 }
